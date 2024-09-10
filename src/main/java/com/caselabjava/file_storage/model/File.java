a5011a639a0413a80e02f3252e0b17ba003ceaca
1werd <mark.rabushev@mail.ru>
2024-09-10 20:22:26 +0300

    Create File Entity

---
+++ src/main/java/com/caselabjava/file_storage/model/File.java
@@ -0,0 +1,36 @@
+package com.caselabjava.file_storage.model;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor(access= AccessLevel.PRIVATE, force=true)
+@Table(name = "file-storage")
+public class File {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "creation_date")
+    @CreationTimestamp
+    private Date creationDate;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "file_content")
+    @Lob
+    private byte[] fileContent;
+}
