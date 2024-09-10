3d52359dfe10e5014119d3b72fb80bc747fe41ef
1werd <mark.rabushev@mail.ru>
2024-09-10 18:31:10 +0300

    Initial commit

---
+++ src/test/java/com/caselabjava/file_storage/FileStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caselabjava.file_storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
