5364c55ea8e3b2f9f8ad51205102458f77593b5e
1werd <mark.rabushev@mail.ru>
2024-09-11 15:01:48 +0300

    Create FileStorageController

--- src/main/java/com/caselabjava/file_storage/controller/FileStorageController.java
+++ src/main/java/com/caselabjava/file_storage/controller/FileStorageController.java
@@ -1,7 +1,47 @@
 package com.caselabjava.file_storage.controller;
 
-import org.springframework.stereotype.Controller;
+import com.caselabjava.file_storage.model.Storage;
+import com.caselabjava.file_storage.repository.StorageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path="storage", produces="application/json")
 public class FileStorageController {
+
+    private final StorageRepository storageRepository;
+
+    @Autowired
+    public FileStorageController(StorageRepository storageRepository) {
+        this.storageRepository = storageRepository;
+    }
+
+    @PostMapping(consumes="application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Integer postTaco(@RequestBody Storage file) {
+        return storageRepository.save(file).getId();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Storage> getById(@PathVariable("id") Integer id) {
+        Optional<Storage> file = storageRepository.findById(id);
+        if (file.isPresent()) {
+            return new ResponseEntity<>(file.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping
+    public Iterable<Storage> list() {
+        return storageRepository.findAll(PageRequest.of(0, 10,
+                Sort.by("creationDate").descending())).getContent();
+    }
+
+
 }
